987905af3a3e82df4d8af19c1b3c506f4ab3637c
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-02-01 12:06:30 +0900

    simpoeboard service 수정
    findall 메소드에 페이징 수정
    페이징을 위해 JpaRepository -> PagingAndSortingRepository 수정

--- src/main/java/com/samlasoft/simpleboard/domain/SimpleBoardService.java
+++ src/main/java/com/samlasoft/simpleboard/domain/SimpleBoardService.java
@@ -1,22 +1,22 @@
 package com.samlasoft.simpleboard.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class SimpleBoardService {
 
     @Autowired
-    JpaRepository<SimpleBoard,Long> repository;
+    PagingAndSortingRepository<SimpleBoard,Long> repository;
 
     public void save(SimpleBoard simpleBoard){
         repository.save(simpleBoard);
     }
 
-    public List<SimpleBoard> findAll(){
-        return repository.findAll();
+    public Page<SimpleBoard> findAll(Pageable pageable){
+        return repository.findAll(pageable);
     }
 }
