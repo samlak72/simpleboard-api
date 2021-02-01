1495cf0cbe70887e171b0015d9f8efca24544e93
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-02-01 12:04:17 +0900

    simpoeboard repository 수정
    페이징과 정렬을 위해서 PagingAndSortingRepository interface로 수정

--- src/main/java/com/samlasoft/simpleboard/domain/SimpleBoardRepository.java
+++ src/main/java/com/samlasoft/simpleboard/domain/SimpleBoardRepository.java
@@ -1,8 +1,11 @@
 package com.samlasoft.simpleboard.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SimpleBoardRepository extends JpaRepository<SimpleBoard,Long>  {
+public interface SimpleBoardRepository extends PagingAndSortingRepository<SimpleBoard,Long> {
+    Page<SimpleBoard> findAll(Pageable pageable);
 }
