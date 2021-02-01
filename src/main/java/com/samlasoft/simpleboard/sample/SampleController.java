13866de74139f4edc61ab807833c1c8281b60c68
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-02-01 12:02:51 +0900

    simpoeboard controller 메소드 및 클래스 추가
    저장메소드추가
    기존 find메소드 페이징 추가
    api호출 결과 클래스 추가

--- src/main/java/com/samlasoft/simpleboard/sample/SampleController.java
+++ src/main/java/com/samlasoft/simpleboard/sample/SampleController.java
@@ -4,13 +4,18 @@ import com.samlasoft.simpleboard.domain.SimpleBoard;
 import com.samlasoft.simpleboard.domain.SimpleBoardService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 
 @RestController
 public class SampleController {
@@ -20,20 +25,37 @@ public class SampleController {
 
     @RequestMapping("/hello")
     public Hello hello(){
-        SimpleBoard simpleBoard = new SimpleBoard();
-        simpleBoard.setMessage("hello samlak!");
-
-        simpleBoardService.save(simpleBoard);
-
         return new Hello("samlak", "Hello",LocalDate.now(),LocalTime.now());
     }
 
     @RequestMapping("/findAll")
-    public List<SimpleBoard> findAll(){
+    public Page<SimpleBoard> findAll(Pageable pageable){
 
-        return simpleBoardService.findAll();
+        pageable = PageRequest.of(pageable.getPageNumber(),pageable.getPageSize(), Sort.by("id").descending());
+        return simpleBoardService.findAll(pageable);
     }
 
+    @RequestMapping("/saveMessage")
+    public Result saveMessage(@RequestBody SimpleBoard simpleBoard){
+        Result result = new Result(200,"Success!");
+        try{
+            simpleBoardService.save(simpleBoard);
+        }catch(Exception e){
+            e.printStackTrace();
+            result = new Result(500, "Error!");
+        }
+
+        return result;
+    }
+
+}
+
+@Setter
+@Getter
+@AllArgsConstructor
+class Result {
+    int code;
+    String message;
 }
 
 @Getter
