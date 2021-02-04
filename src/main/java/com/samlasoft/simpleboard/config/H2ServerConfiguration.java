b55d818c5ea7b50aad9ea5cb317b211b8408c759
DESKTOP-HMERR5D\samlak <samlak72@gmail.com>
2021-02-04 22:51:42 +0900

    테스트용 db 생성 추가
    테스트케이스 작성후 테스트시 이미 포트가 있다는 오류발생
    h2db를 어플리케이션 시작시 생성하는데 이미 서버가 떠있는경우 포트 충돌로 테스트가 제대로 안됨
    application-test.yml를 추가하고 새로운 테스트용 db 생성

--- src/main/java/com/samlasoft/simpleboard/config/H2ServerConfiguration.java
+++ src/main/java/com/samlasoft/simpleboard/config/H2ServerConfiguration.java
@@ -1,6 +1,8 @@
 package com.samlasoft.simpleboard.config;
 
+import lombok.extern.java.Log;
 import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,14 +11,20 @@ import org.springframework.context.annotation.Profile;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
+@Log
 @Configuration
 @Profile("local")
 public class H2ServerConfiguration {
 
+    @Value("${spring.h2.port}")
+    String port;
+
     @Bean
     @ConfigurationProperties("spring.datasource.hikari")
     public DataSource dataSource() throws SQLException {
-        Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
+
+        log.info("port: "+port);
+        Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", port).start();
 
         return new com.zaxxer.hikari.HikariDataSource();
     }
